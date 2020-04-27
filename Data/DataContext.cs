079dc70bc35ef923a314554ac5c8a6c25f00e006
gbubemismith <gsmithwalter@gmail.com>
2020-04-27 22:23:41 +0100

    first commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+
+using CommunitySocial.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommunitySocial.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options )
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ Models/ Value.cs
@@ -0,0 +1,9 @@
+namespace CommunitySocial.API.Models
+{
+    
+    public class  Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }         
+    }
+}
