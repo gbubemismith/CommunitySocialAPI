65561ac1427e51b08a8fa7cb57fea0c16452e402
gbubemismith <gsmithwalter@gmail.com>
2020-06-08 23:38:31 +0100

    Added filter properties to GetUsers API

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -30,6 +30,18 @@ namespace CommunitySocial.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams) 
         {
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            var userFromRepo = await _repo.GetUser(currentUserId);
+
+            userParams.UserId = currentUserId;
+            
+            if (string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = userFromRepo.Gender == "male" ? "female" : "male";
+            }
+
+            
             var users = await _repo.GetUsers(userParams);
 
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
