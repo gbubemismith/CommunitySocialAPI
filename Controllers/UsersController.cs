2ded97457734b8472525e5f6f8757ea5fcc66c4f
gbubemismith <gsmithwalter@gmail.com>
2020-06-11 23:33:21 +0100

    Added like entity and functionality

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using AutoMapper;
 using CommunitySocial.API.Data;
 using CommunitySocial.API.Dtos;
 using CommunitySocial.API.Helpers;
+using CommunitySocial.API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -75,6 +76,34 @@ namespace CommunitySocial.API.Controllers
                 return NoContent();
 
             throw new Exception($"Updating user {id} failed on save");
+        }
+
+        [HttpPost("{id}/like/{recipientId}")]
+        public async Task<IActionResult> LikeUser(int id, int recipientId)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var like = await _repo.GetLike(id, recipientId);
+
+            if (like != null)
+                return BadRequest("You already like this user");
+
+            if (await _repo.GetUser(recipientId) == null)
+                return NotFound();
+
+            like = new Like
+            {
+                LikerId = id,
+                LikeeId = recipientId
+            };
+
+            _repo.Add<Like>(like);
+
+            if (await _repo.SaveAll())
+                return Ok();
+
+            return BadRequest("Failed to like user");
         }
     }
 }
--- Data/ISocialRepository.cs
+++ Data/ISocialRepository.cs
@@ -14,6 +14,7 @@ namespace CommunitySocial.API.Data
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
          Task<Photo> GetMainPhotoForUser(int userId);
+         Task<Like> GetLike(int userId, int recipientId);
          
     }
 }
